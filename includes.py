c31ca694ebf9cb19e4595d89c26c73f8cc37b9bc
zhurai <zhurai.tsuki@gmail.com>
2022-08-26 15:53:41 -0700

    initial commit to convert all scripts to python

---
+++ includes.py
@@ -0,0 +1,48 @@
+#!/bin/python3
+import subprocess
+
+def date_create (date_format,day,adjustments=None):
+    if adjustments:
+        result=subprocess.run(['date','-d',day+adjustments,date_format], stdout=subprocess.PIPE)
+    else:
+        result=subprocess.run(['date','-d',day,date_format])
+    thedate=result.stdout.decode('utf-8').rstrip()
+    return thedate
+
+def task_add (rc_dateformat,task_name,projects,tags,priority=0,scheduled=None,until=None,due=None,start=None,annotations=None,depends=None):
+    # run task command
+    subprocess.run(['task','add','project:"'+projects+'"',*tags,task_name,'priority:"'+str(priority)+'"','rc.dateformat:"'+rc_dateformat+'"'])
+    
+    # get last id (for more actions)
+    result=subprocess.run(['task','+LATEST','ids'], stdout=subprocess.PIPE)
+    task_id=result.stdout.decode('utf-8').rstrip()
+    
+    # add start
+    if start:
+        subprocess.run(['task',task_id,'modify','start:"'+start+'"'], stdout=subprocess.PIPE)
+    
+    # add scheduled
+    if scheduled:
+        subprocess.run(['task',task_id,'modify','scheduled:"'+scheduled+'"'], stdout=subprocess.PIPE)
+    
+    # add due
+    if due:
+        subprocess.run(['task',task_id,'modify','due:"'+due+'"'], stdout=subprocess.PIPE)
+
+    # add until
+    if until:
+        subprocess.run(['task',task_id,'modify','until:"'+until+'"'], stdout=subprocess.PIPE)
+
+    # add annotations (Array)
+    if annotations:
+        for item in annotations:
+            subprocess.run(['task',task_id,'annotate',item], stdout=subprocess.PIPE)
+    
+    # add dependencies (Array)
+    if depends:
+        for item in depends:
+            subprocess.run(['task',task_id,'modify','depends:"'+item+'"'], stdout=subprocess.PIPE)
+
+if __name__ == "__main__":
+   # stuff only to run when not called via 'import' here
+   pass
