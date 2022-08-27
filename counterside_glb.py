d55c1f7b20375900ef008571a767698110b1ba29
zhurai <zhurai.tsuki@gmail.com>
2022-08-27 16:49:23 -0700

    update main counterside scripts

--- counterside_glb.py
+++ counterside_glb.py
@@ -4,7 +4,8 @@ project="Games.CounterSideGLB"
 tags=["+games","+counterside","+countersideglb"]
 base_title="CounterSide.GLB: "
 
-offset=+5 # +5 = UTC-5
+offset=-9 # -9 = UTC+9
+login_offset=+5 # +5 = UTC-5
 daily_reset="4:00"
 daily_shop_reset="0:00"
 weekly_reset="monday 4:00"
--- counterside_sea.py
+++ counterside_sea.py
@@ -4,7 +4,7 @@ project="Games.CounterSideSEA"
 tags=["+games","+counterside","+countersidesea"]
 base_title="CounterSide.SEA: "
 
-offset=-7 # +7 = UTC-7
+offset=-7 # -7 = UTC+7
 daily_reset="4:00"
 daily_shop_reset="0:00"
 weekly_reset="monday 4:00"
