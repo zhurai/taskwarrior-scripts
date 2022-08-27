57a126ccf8e72d4620f6a1f9d7dd5f3d7dae95f7
zhurai <zhurai.tsuki@gmail.com>
2022-08-26 19:13:28 -0700

    weekly arknights-annihilation update

--- weekly/arknights-annihilation.py
+++ weekly/arknights-annihilation.py
@@ -22,25 +22,47 @@ loop=5
 
 # Other Variables
 rc_dateformat="Y-M-DTH:N:S"
-idarray=()
+id_array=()
 date_format="+%Y-%m-%dT%H:%M:%S"
 
 if sys.argv == 1:
-	# no command line arguments, then assume we are building for an upcoming week
-	schedule_=includes.date_create(date_format,schedule)
-	due_temp=includes.date_create("+%Y-%m-%d",due)
-	due_=includes.date_create(date_format,due_temp,"+7 days")
-	until_=includes.date_create(date_format,until)
+    # no command line arguments, then assume we are building for an upcoming week
+    schedule_=includes.date_create(date_format,schedule)
+    due_temp=includes.date_create("+%Y-%m-%d",due)
+    due_=includes.date_create(date_format,due_temp,"+7 days")
+    until_=includes.date_create(date_format,until)
 else:
-	# if any commandline arguments, then assume we are building for this week
-	schedule_=includes.date_create(date_format,schedule)
-	due_=includes.date_create(date_format,due)
-	until_=includes.date_create(date_format,until)
+    # if any commandline arguments, then assume we are building for this week
+    schedule_=includes.date_create(date_format,schedule)
+    due_=includes.date_create(date_format,due)
+    until_=includes.date_create(date_format,until)
 
 for x in range(1,loop+1):
-	# create title
-	new_title="\""+base_title+": Run " + x + "/" + loop+"\""	
-	
-	# Add the task and get lastid for the array
-	includes.task_add(rc_dateformat,new_title,project,tags,priority,schedule_,until_,due_)
-	lastid=includes.task_get_last()	
+    # create title
+    new_title="\""+base_title+": Run " + x + "/" + loop+"\""	
+    
+    # create dependency array
+    depends_array=[]
+    depends="depends:"
+    for id in id_array:
+        if id is "":
+            pass
+        else:
+            depends_array.append("depends:"+id)
+
+    # Add the task and get lastid for the array
+    includes.task_add(rc_dateformat,new_title,project,tags,priority,schedule_,until_,due_,None,None,depends_array)
+    lastid=includes.task_get_last()	
+    id_array.append(lastid)
+
+# create dependency array
+depends_array=[]
+depends="depends:"
+for id in id_array:
+    if id is "":
+        pass
+else:
+    depends_array.append("depends:"+id)
+    
+# After all of the "series" is added, make the "category"
+includes.task_add(rc_dateformat,base_title,project,tags,priority,schedule_,until_,None,None,None,depends_array)
