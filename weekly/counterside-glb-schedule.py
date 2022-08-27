6a260e7e2c1d8c3da805ab227f131c8e9f9aa675
zhurai <zhurai.tsuki@gmail.com>
2022-08-27 16:51:14 -0700

    add cs global weeklies

---
+++ weekly/counterside-glb-schedule.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+import os
+file_path = os.path.dirname(os.path.abspath(__file__))
+import sys
+sys.path.insert(0, file_path+"/..")
+from includes import *
+from counterside_glb import *
+
+# Event information
+tags.append('+schedule')
+priority=-1
+
+def create_date (title,scheduled,until,annotations=None):
+    # the dates should be the source timezone
+    scheduled_=date_create(date_format,scheduled,None,offset)
+    until_=date_create(date_format,until,None,offset)
+    
+    # update title
+    new_title="\""+base_title+title+"\"" 
+    
+    # execute command
+    if annotations:
+        task_add(rc_dateformat,new_title,project,tags,priority,scheduled_,until_,None,None,annotations,None)
+    else:
+        task_add(rc_dateformat,new_title,project,tags,priority,scheduled_,until_,None,None,None,None)
+
+#################################################################
+
+# Weekly Reset
+#  scheduled:  	monday 4:00
+#  until:		next monday 4:00
+thetitle="Reset: Weekly"
+scheduled=weekly_reset
+until="next "+weekly_reset
+create_date(thetitle,scheduled,until) 
+
+# Weekly Shop Reset
+#  scheduled:  	monday 0:00
+#  until:		next monday 0:00
+thetitle="Reset: Weekly Shop"
+scheduled=weekly_shop_reset
+until="next "+weekly_shop_reset
+annotations=["Reset: Store: Exchange: Convenience: Gear Precision Package (3qty, 790quartz) [Low Priority]",
+    "Reset: Store: Exchange: Convenience: Fusion Core (2qty, 260quartz) [Medium Priority]",
+    "Reset: Store: Exchange: Dive: Needed Imaginary Core Items [High Priority]",
+    "Reset: Store: Exchange: ShadowPalace: Ultraprecise T6 Spectral Universal Mold (1qty, 120shards) [High Priority]",
+    "Reset: Store: Exchange: ShadowPalace: 75% Off T6 Spectral Universal Mold (15qty, 15shards) [High Priority]",
+    "Reset: Store: Exchange: ShadowPalace: 50% Off T6 Spectral Universal Mold (15qty, 30shards) [High Priority]",
+    "Reset: Store: Exchange: ShadowPalace: Tuning Binary (30qty, 5shards) [High Priority]",
+    "Reset: Store: Exchange: Consortium: Set Binary (5qty,800tokens) [High Priority]",
+    "Reset: Store: Exchange: Consortium: Fusion Core (1qty,2000tokens) [Medium Priority]",
+    "Reset: Store: Exchange: Consortium: S-APT Core Coupon (3qty,1000tokens) [Low Priority]",
+    "Reset: Store: Exchange: BusinessCard: 10 APT Cores Coupon (3qty,10cards) [High Priority]",
+    "Reset: Store: Exchange: BusinessCard: S-APT Cores Coupon (10qty,10cards) [High Priority]",
+    "Reset: Store: Exchange: BusinessCard: Fusion Core (5qty,30cards) [High Priority]"]
+create_date(thetitle,scheduled,until,annotations) 
---
+++ weekly/counterside-glb-shadowpalace.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+import os
+file_path = os.path.dirname(os.path.abspath(__file__))
+import sys
+sys.path.insert(0, file_path+"/..")
+from includes import *
+from counterside_glb import *
+
+# Date generation
+#  start:		
+#  scheduled:  	monday 4:00
+#  due: 		sunday 23:59
+#  until:		next monday 4:00
+schedule=weekly_reset
+due="sunday 23:59"
+until="next "+weekly_reset
+
+# Event information
+tags.append('+weeklies')
+base_title="Weekly Shadow Palace"
+priority=2
+loop=7
+
+# Other Variables
+id_array=[]
+
+if sys.argv == 1:
+    # no command line arguments, then assume we are building for an upcoming week
+    schedule_=date_create(date_format,schedule)
+    due_temp=date_create("+%Y-%m-%d",due)
+    due_=date_create(date_format,due_temp,"+7 days")
+    until_=date_create(date_format,until)
+else:
+    # if any commandline arguments, then assume we are building for this week
+    schedule_=date_create(date_format,schedule)
+    due_=date_create(date_format,due)
+    until_=date_create(date_format,until)
+
+for interval in range(1,loop+1):
+    # create title
+    new_title="\""+base_title+": Run "+str(interval)+"/"+str(loop)+"\""	
+    
+    # create dependency array
+    depends=[]
+    for id in id_array:
+        if id == "":
+            pass
+        else:
+            depends.append(str(id))
+    
+    # Add the task and get lastid for the array
+    task_add(rc_dateformat,new_title,project,tags,priority,schedule_,until_,due_,None,None,depends)
+    lastid=task_get_last()	
+    id_array.append(lastid)
+
+# create dependency array
+depends=[]
+for id in id_array:
+    if id == "":
+        pass
+else:
+    depends.append(str(id))
+    
+# After all of the "series" is added, make the "category"
+task_add(rc_dateformat,base_title,project,tags,priority,schedule_,until_,None,None,None,depends)
